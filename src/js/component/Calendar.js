c8c60b8682ef8830c0058de2c3eec71d77e79f9a
Edward Xiao <edwardfhsiao@gmail.com>
2019-07-10 18:23:30 +0800

    no message

--- src/js/component/Calendar.js
+++ src/js/component/Calendar.js
@@ -471,7 +471,7 @@ const CalendarItem = memo(({ item = {}, isPicked = false, isDisabled = false, da
       {isPicked && (
         <svg className="picky-date-time-check" width="15" height="15" viewBox="0 0 24 24">
           <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+          <path className="picky-date-time-check__path" d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
         </svg>
       )}
     </div>
